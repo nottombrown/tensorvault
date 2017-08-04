ef26031503d762cbe518d81aa97688da078f70c6
Tom Brown <nottombrown@gmail.com>
2017-08-03 19:00:18 -0700

    Update tests

--- cli_test.py
+++ cli_test.py
@@ -1,5 +1,14 @@
+from path import Path
+
 from tf_scribe.cli import collect_git_data
 
-def test_collect_git_data():
+def test_collect_git_data_local():
     git_data = collect_git_data()
-    assert git_data['commit'] == "[unknown]"
+    assert git_data['commit'] != "[unknown]"
+    assert git_data['repository'] != "[unknown]"
+
+def test_collect_git_data_no_repo():
+    with Path('/tmp'):
+        git_data = collect_git_data()
+        assert git_data['commit'] == "[unknown]"
+        assert git_data['repository'] == "[unknown]"
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 
 from setuptools import setup
+
 if sys.version_info.major != 3:
     print("This module is only compatible with Python 3, but you are running "
           "Python {}. The installation will likely fail.".format(sys.version_info.major))
@@ -14,6 +15,10 @@ setup(name='tf_scribe',
     extras_require={
         "tf": ["tensorflow ~= 1.2"],
         "tf_gpu": ["tensorflow-gpu >= 1.1"],
+        "test": [
+            'pytest ~= 1.4.33',
+            'path.py ~= 10.3.1'
+        ]
     },
     scripts=['bin/scribe']
 )
