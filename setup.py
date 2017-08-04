1295f322aa8aa38a4a9bf182b9780acd1bca04d1
Tom Brown <nottombrown@gmail.com>
2017-08-03 18:04:30 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+
+from setuptools import setup
+if sys.version_info.major != 3:
+    print("This module is only compatible with Python 3, but you are running "
+          "Python {}. The installation will likely fail.".format(sys.version_info.major))
+
+setup(name='tf_scribe',
+    version='0.0.1',
+    install_requires=[
+        "click ~= 6.0"
+    ],
+    # https://github.com/tensorflow/tensorflow/issues/7166#issuecomment-280881808
+    extras_require={
+        "tf": ["tensorflow ~= 1.2"],
+        "tf_gpu": ["tensorflow-gpu >= 1.1"],
+    },
+    scripts=['bin/scribe']
+)
