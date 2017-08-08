e69753a25bef29433a272d692ed6c32aebd8878a
Tom Brown <nottombrown@gmail.com>
2017-08-08 12:45:07 -0700

    Update reqs

--- setup.py
+++ setup.py
@@ -8,14 +8,15 @@ if sys.version_info.major != 3:
 
 setup(
     name='tensorvault',
-    packages = ['tensorvault'],
+    packages=['tensorvault'],
     version='0.0.1',
     install_requires=[
-        "path.py==10.3"
+        "path.py ~= 10.3",
+        "pyyaml ~= 3.12",
     ],
     author='Tom B Brown',
     author_email='nottombrown@gmail.com',
-    url = 'https://github.com/nottombrown/tensorvault',
+    url='https://github.com/nottombrown/tensorvault',
     # https://github.com/tensorflow/tensorflow/issues/7166#issuecomment-280881808
     extras_require={
         "tf": ["tensorflow ~= 1.2"],
